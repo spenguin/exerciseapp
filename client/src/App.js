9db5b17367775b6cd6fff330e01a62d9229a5804
John Anderson <info@soaringpenguin.com>
2022-01-11 19:14:25 -0800

    Add a few more dependencies and a style library

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js called from index.js
+
+// Import node components 
+import { Component } from "react";
+import { 
+  BrowserRouter, 
+  Switch, 
+  Route, 
+  Redirect } from 'react-router-dom';
+
+// import build pages
+
+// import SCSS
+import "../src/styles/app.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
